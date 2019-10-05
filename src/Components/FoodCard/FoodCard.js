9d18467ae25ec8fd502ac5022bb93e015c07820a
tripolarbears <charlespkehler@gmail.com>
2019-10-05 15:40:34 -0400

    mvp

--- src/Components/FoodCard/FoodCard.js
+++ src/Components/FoodCard/FoodCard.js
@@ -1,15 +1,59 @@
 import React, { Component } from "react";
 import "./FoodCard.css";
+
 class FoodCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selected: false
+      selected: {},
+      data: [
+        "Burgers",
+        "Chinese",
+        "French",
+        "Indian",
+        "Italian",
+        "Korean",
+        "Mexican",
+        "Pizza",
+        "Ramen",
+        "Spanish",
+        "Sushi",
+        "Thai"
+      ],
+      picked: ""
     };
+    this.selectItem = this.selectItem.bind(this);
+    this.renderItem = this.renderItem.bind(this);
   }
   handleClick(i, event) {
     alert(i);
+    this.setState({ selected: true });
   }
+  selectItem(el) {
+    var selected = this.state.selected;
+    selected[el] = !selected[el];
+    this.setState({ selected: selected });
+  }
+  renderItem(el) {
+    var className = this.state.selected[el] ? "active" : "inactive";
+    var onClick = this.selectItem.bind(this, el);
+    return (
+      <div
+        className="foodcard"
+        key={el}
+        className={`${className} foodcard`}
+        onClick={onClick}
+      >
+        {el}
+      </div>
+    );
+  }
+  handleButton = () => {
+    let foodOptions = Object.keys(this.state.selected);
+    this.setState({
+      picked: foodOptions[Math.floor(Math.random() * foodOptions.length)]
+    });
+  };
 
   render() {
     let foodList = [
@@ -28,18 +72,12 @@ class FoodCard extends Component {
     ];
     return (
       <React.Fragment>
+        <ul></ul>
         <div className="food-cards-container">
-          {foodList.map((item, i) => (
-            <div
-              key={i}
-              onClick={this.handleClick.bind(this, i)}
-              className="foodcard"
-            >
-              {item}
-            </div>
-          ))}
+          {this.state.data.map(this.renderItem)}
         </div>
-        <button>Pick for me!</button>
+        <button onClick={this.handleButton}>Pick for me!</button>
+        <div>{this.state.picked}</div>
       </React.Fragment>
     );
   }
